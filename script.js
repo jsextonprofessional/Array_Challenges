529cd68fed7d3f338fbaa59ac56b764e85594582
jsprofessional <jshuman@protonmail.com>
2021-07-05 18:11:57 -0500

    solved all 5

--- script.js
+++ script.js
@@ -1,62 +1,63 @@
-// // 1. Always Hungry
-// // Write a function that is given an array and each time the value is "food" it should console log "yummy". If "food" was not present in the array console log "I'm hungry" once.
-// function hungry(arr) {
-//     var food = 0;
-//     for (var i = 0; i < arr.length; i++) {
-//         if (arr[i] == "food") {
-//             console.log("yummy");
-//             food++;
-//         }
-//     }
-//     if (food == 0) {
-//         console.log("I'm hungry")
-//     } 
-// }
-// hungry([3.14, "food", "pie", true, "food"]);
-// // this should console log "yummy", "yummy"
-// hungry([4, 1, 5, 7, 2]);
+// 1. Always Hungry
+// Write a function that is given an array and each time the value is "food" it should console log "yummy". If "food" was not present in the array console log "I'm hungry" once.
+function hungry(arr) {
+    var food = 0;
+    for (var i = 0; i < arr.length; i++) {
+        if (arr[i] == "food") {
+            console.log("yummy");
+            food++;
+        }
+    }
+    if (food == 0) {
+        console.log("I'm hungry")
+    } 
+}
+hungry([3.14, "food", "pie", true, "food"]);
+// this should console log "yummy", "yummy"
+hungry([4, 1, 5, 7, 2]);
 // this should console log "I'm hungry"
 
 
 // 2. High Pass Filter
 // Given an array and a value cutoff, return a new array containing only the values larger than cutoff.
-// function highPass(arr, cutoff) {
-//     var filteredArr = [];
-//     for (var i = 0; i < arr.length; i++) {
-//         if (arr[i] > cutoff) {
-//             filteredArr.push(arr[i]);
-//         }
-//     }
-//     return filteredArr;
-// }
-// var result = highPass([6, 8, 3, 10, -2, 5, 9], 5);
-// console.log(result); // we expect back [6, 8, 10, 9]
+
+function highPass(arr, cutoff) {
+    var filteredArr = [];
+    for (var i = 0; i < arr.length; i++) {
+        if (arr[i] > cutoff) {
+            filteredArr.push(arr[i]);
+        }
+    }
+    return filteredArr;
+}
+var result = highPass([6, 8, 3, 10, -2, 5, 9], 5);
+console.log(result); // we expect back [6, 8, 10, 9]
 
 
-// // 3. Better than average
-// // Given an array of numbers return a count of how many of the numbers are larger than the average.
+// 3. Better than average
+// Given an array of numbers return a count of how many of the numbers are larger than the average.
 
-// // function to determine average
-// function betterThanAverage(arr) {
-//     var sum = 0;
+// function to determine average
+function betterThanAverage(arr) {
+    var sum = 0;
     
-//     for (var i = 0; i < arr.length; i++) {
-//         sum += arr[i];
-//     }
+    for (var i = 0; i < arr.length; i++) {
+        sum += arr[i];
+    }
     
-//     var avg = sum / arr.length;
-//     var count = 0;
+    var avg = sum / arr.length;
+    var count = 0;
     
-//     // count how many values are greated than the average
-//     for (var i = 0; i < arr.length; i++) {
-//         if (arr[i] > avg) {
-//             count++;
-//         }
-//     }
-//     return count;
-// }
-// var result = betterThanAverage([6, 8, 3, 10, -2, 5, 9]);
-// console.log(result); // we expect back 4
+    // count how many values are greated than the average
+    for (var i = 0; i < arr.length; i++) {
+        if (arr[i] > avg) {
+            count++;
+        }
+    }
+    return count;
+}
+var result = betterThanAverage([6, 8, 3, 10, -2, 5, 9]);
+console.log(result); // we expect back 4
 
 
 // 4. Array Reverse
@@ -78,14 +79,18 @@ var result = reverse(["a", "b", "c", "d", "e"]);
 console.log(result); // we expect back ["e", "d", "c", "b", "a"]
 
 
-// // 5. Fibonacci Array
-// // Fibonacci numbers have been studied for years and appear often in nature. Write a function that will return an array of Fibonacci numbers up to a given length n. Fibonacci numbers are calculated by adding the last two values in the sequence together. So if the 4th value is 2 and the 5th value is 3 then the next value in the sequence is 5.
-// function fibonacciArray(n) {
-//     // the [0, 1] are the starting values of the array to calculate the rest from
-//     var fibArr = [0, 1];
-//     // your code here
-//     return fibArr;
-// }
+// 5. Fibonacci Array
+// Fibonacci numbers have been studied for years and appear often in nature. Write a function that will return an array of Fibonacci numbers up to a given length n. Fibonacci numbers are calculated by adding the last two values in the sequence together. So if the 4th value is 2 and the 5th value is 3 then the next value in the sequence is 5.
+function fibonacciArray(n) {
+    // the [0, 1] are the starting values of the array to calculate the rest from
+    var fibArr = [0, 1];
+    while (fibArr.length < n) {
+        var prev1 = fibArr[fibArr.length - 1];
+        var prev2 = fibArr[fibArr.length - 2];
+        fibArr.push(prev1 + prev2);
+    }
+    return fibArr;
+}
    
-// var result = fibonacciArray(10);
-// console.log(result); // we expect back [0, 1, 1, 2, 3, 5, 8, 13, 21, 34]
+var result = fibonacciArray(10);
+console.log(result); // we expect back [0, 1, 1, 2, 3, 5, 8, 13, 21, 34]
