022ba4c3f28eb171358e6df933838ddc85070ddf
jsprofessional <jshuman@protonmail.com>
2021-07-05 17:46:24 -0500

    solved 3

--- script.js
+++ script.js
@@ -35,19 +35,24 @@
 
 // 3. Better than average
 // Given an array of numbers return a count of how many of the numbers are larger than the average.
+
 // function to determine average
-// function
 function betterThanAverage(arr) {
     var sum = 0;
+    
     for (var i = 0; i < arr.length; i++) {
-        sum += i;
+        sum += arr[i];
     }
-    var avg = sum/arr.length;
+    
+    var avg = sum / arr.length;
     var count = 0;
-    if (arr[i] > avg) {
-        count ++;
-    }
+    
     // count how many values are greated than the average
+    for (var i = 0; i < arr.length; i++) {
+        if (arr[i] > avg) {
+            count++;
+        }
+    }
     return count;
 }
 var result = betterThanAverage([6, 8, 3, 10, -2, 5, 9]);
